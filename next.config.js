1bf6dc474d7c216c9734d6987dd701de76fe4258
Tomás Philippart <tomas.philippart@ing.com>
2024-06-20 10:50:08 +0100

    Rollback config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,2 @@
-module.exports = {
-    output: 'export',
-    distDir: 'out',
-    basePath: "/TomasPhilippart",
-    images: {
-        unoptimized: true,
-      },
-    // other Next.js configurations
-  }
+module.exports = {}
   

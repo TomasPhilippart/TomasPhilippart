551c0e9bec321decd45b2c33ab408a031717eff6
Tomás Philippart <tomas.philippart@ing.com>
2024-06-20 10:42:41 +0100

    Fixed config for github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     output: 'export',
     distDir: 'out',
+    basePath: "/TomasPhilippart",
+    images: {
+        unoptimized: true,
+      },
     // other Next.js configurations
   }
   
